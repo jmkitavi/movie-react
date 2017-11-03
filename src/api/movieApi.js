501c9e8f3497455a30b49a466cc9aec58eb64aa6
sirjmkitavi <kitavimjoseph@gmail.com>
2017-11-03 13:19:27 +0300

    Minor fixes

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -10,8 +10,16 @@ const addPosterUrl = (movie) => {
       movie.genre_names.push(genre.name)
     })
   }
+  if (!movie.poster_path) {
+    movie.poster_url = `https://i.imgur.com/hwo0ca2.png`; // 550x750 place holder
+  } else {
     movie.poster_url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  }
+  if (!movie.backdrop_path) {
+    movie.backdrop_url = `https://i.imgur.com/H2Z8el2.png`; // 360x202 place holder
+  } else {
     movie.backdrop_url = `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`;
+  }
   movie.release = moment(movie.released_date).format('ll');
   return movie;
 }
